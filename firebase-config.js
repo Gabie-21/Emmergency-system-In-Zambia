f16551f3ce28e0e70a3310f585d4a29dd5ff08f0
Gabie-21 <ngalasagabriella@gmail.com>
2025-08-19 18:45:13 +0300

    Updated the script file by improving the firebase code. I also made changes to the index file and the style file.

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+            const firebaseConfig = {
+                apiKey: "AIzaSyATFnJ_nufdoN4QAcYLjxsZd-eANz988B8",
+                authDomain: "zambia-emergency-response.firebaseapp.com",
+                projectId: "zambia-emergency-response",
+                storageBucket: "zambia-emergency-response.firebasestorage.app",
+                messagingSenderId: "746207632001",
+                appId: "1:746207632001:web:11754eaed394e960708b80",
+                measurementId: "G-BLS7RYQKVB"
+            };
+
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
